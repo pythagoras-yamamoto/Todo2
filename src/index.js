9d48415f6a5395f1bfbf2c74e961fd7fd0ce053b
yudai yamamoto <60994397+pythagoras-yamamoto@users.noreply.github.com>
2021-09-06 21:23:57 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+
+const onClick = () => {
+  //inputの中身を取得し、入力欄を初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //未完了のTODOにinputした内容を生成
+  const div = document.createElement("div");
+  div.className = "list-low";
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  div.appendChild(li);
+
+  //完了ボタンの生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  div.appendChild(completeButton);
+
+  //削除ボタンの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  //deleteButton.addEventListener("click",() => {});
+
+  div.appendChild(deleteButton);
+
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClick());
